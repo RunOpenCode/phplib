91e46094ca5ee1be13f1834cf26ab9df5d88117b
Nikola Svitlica a.k.a TheCelavi <thecelavi@gmail.com>
2025-12-01 10:19:38 +0100

    Frozen API

--- bin/ci.py
+++ bin/ci.py
@@ -31,7 +31,7 @@ def ci(verbose, skip_install, teardown):
 
     commands = {
         'composer install': ('XDEBUG_MODE=off composer install', False),
-        'composer run phpunit': ('composer run phpunit', False),
+        'composer run phpunit': ('composer run phpunit -- --colors=always --testdox', False),
         'composer run phpcs': ('composer run phpcs', False),
         'composer run phpstan': ('composer run phpstan', False),
         'composer run deps': ('composer run phpstan', False),    
